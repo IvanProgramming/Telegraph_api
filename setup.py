5f142bd5de8e11b337e2711e9b9275ad1201c8b2
IvanProgramming <vlasov.ivan.2004@gmail.com>
2021-07-22 00:03:09 +0300

    Version Changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="telegraph_api",
-    version="1.0.3",
+    version="1.0.4",
     author="ivanprogramming",
     description="Asynchronus Python wrapper for telgra.ph API",
     long_description=long_description,
