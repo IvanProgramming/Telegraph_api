1fde3ba3c2d41c04eee662061eeca56eb51ca45c
Ivan <23701564+IvanProgramming@users.noreply.github.com>
2022-07-31 20:02:24 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="telegraph_api",
-    version="1.0.5",
+    version="1.0.6",
     author="ivanprogramming",
     description="Asynchronus Python wrapper for telgra.ph API",
     long_description=long_description,
